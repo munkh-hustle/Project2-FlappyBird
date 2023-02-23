0719fd0be6cba2ef67efd109fbedd7c51c6dc880
munkh-hustle <93699468+munkh-hustle@users.noreply.github.com>
2023-02-24 02:15:17 +0800

    adding word effect to restart button

--- game.js
+++ game.js
@@ -197,31 +197,35 @@ function loop() {
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
 
-document.querySelector("h2").onmouseover = event => {  
-  let iteration = 0;
-  
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
-      
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
+function wordEffect(name) {
+    let interval = null;
+    document.querySelector(name).onmouseover = event => {
+        let iteration = 0;
+
+        clearInterval(interval);
+
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration) {
+                        return event.target.dataset.value[index];
+                    }
+
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+
+            if (iteration >= event.target.dataset.value.length) {
+                clearInterval(interval);
+            }
+
+            iteration += 1 / 3;
+        }, 30);
     }
-    
-    iteration += 1 / 3;
-  }, 30);
 }
 
+wordEffect("h2");
+wordEffect("button");
 loop();
